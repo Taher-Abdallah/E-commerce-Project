582bfb0ac161bc4a0efceaaabd912cfe74456037
Taher <Tabdallah173@gmail.com>
2025-09-14 15:18:23 +0200

    feat:making Model And Migrations

--- database/migrations/0001_01_01_000003_create_users_table.php
+++ database/migrations/0001_01_01_000003_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->boolean('status')->default(1);
+            $table->foreignId('city_id')->constrained('cities')->cascadeOnDelete();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
